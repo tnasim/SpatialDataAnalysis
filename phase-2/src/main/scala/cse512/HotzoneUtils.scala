3c7f0b76e1ee84ec2702bb78decfbcf5b52b47a1
saurabhgaggar <saurabhgaggar27@gmail.com>
2021-04-16 19:16:54 +0530

    adding st contains code from pahse 1

--- phase-2/src/main/scala/cse512/HotzoneUtils.scala
+++ phase-2/src/main/scala/cse512/HotzoneUtils.scala
@@ -4,32 +4,25 @@ object HotzoneUtils {
 
   def ST_Contains(queryRectangle: String, pointString: String ): Boolean = {
     // YOU NEED TO CHANGE THIS PART
+    //ST_Contains code from phase 1
+    if (queryRectangle == null || pointString == null || pointString.isEmpty()|| queryRectangle.isEmpty())
+      return false
 
-    //rectangle (x1,y1,x2,y2) coordinates
-    val rectangleVal = queryRectangle.split(",")
-    val x1 = rectangleVal(0).toDouble
-    val y1 = rectangleVal(1).toDouble
-    val x2 = rectangleVal(2).toDouble
-    val y2 = rectangleVal(3).toDouble
+    val rectSplitArray = queryRectangle.split(",").map(_.toDouble)
+    val topLeftX = rectSplitArray(0)
+    val topLeftY = rectSplitArray(1)
+    val botRgtX = rectSplitArray(2)
+    val botRgtY = rectSplitArray(3)
 
-    val minX = math.min(x1,x2)
-    val minY = math.min(y1,y2)
-    val maxX = math.max(x1,x2)
-    val maxY = math.max(y1,y2)
+    val pointStringSplitArray = pointString.split(",").map(_.toDouble)
+    val pointX = pointStringSplitArray(0)
+    val pointY = pointStringSplitArray(1)
 
-    //point (a,b) coordinates
-    val pointStringVal = pointString.split(",")
-    val pointA = pointStringVal(0).toDouble
-    val pointB = pointStringVal(1).toDouble
-
-    if(minX <= pointA && maxX >= pointA && minY <= pointB && maxY >= pointB) {
+    if ((pointX >= topLeftX && pointX <= botRgtX && pointY >= topLeftY && pointY <= botRgtY) ||
+      (pointX >= botRgtX && pointX <= topLeftX && pointY >= botRgtY && pointY <= topLeftY)) {
       return true
     }
-
-    else {
-      return false
-    }
-
+    return false
   }
 
 }
